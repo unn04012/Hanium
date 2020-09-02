8774fda34b8031f97b357bf60fdfb97c98f5f0f4
unn04012 <unn04012@naver.com>
2020-09-02 23:10:56 +0900

    modify

--- search.php
+++ search.php
@@ -2,13 +2,12 @@
 include('./dbconnect.php');
 if(isset($_POST['userName'])){
     $id = $_POST['userName'];
-    $sql = "SELECT * FROM problem_check WHERE user_id = '$id'";
+    $sql = "SELECT * FROM user WHERE user_id = '$id'";
     $stmt = $pdo->prepare($sql);
     $stmt -> execute();
     $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
     $result = json_encode($result);
-
     echo $result;
 }
  ?>

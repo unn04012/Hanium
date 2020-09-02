330ce0efcfa407453600384e4f85c31717445232
unn04012 <unn04012@naver.com>
2020-09-02 22:45:58 +0900

    init

--- dbconnect.php
+++ dbconnect.php
@@ -1,4 +1,4 @@
 <?php
-$pdo = new PDO('mysql:host=localhost;dbname=Hanium;charset=utf8','root','1234');
+$pdo = new PDO('mysql:host=localhost;dbname=Hanium;charset=utf8','qwe123','qwe123');
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
  ?>

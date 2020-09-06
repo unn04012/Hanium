3f328e086a0639d41a24c317795fb94e50ec5236
unn04012 <unn04012@naver.com>
2020-09-06 22:54:02 +0900

    수정

--- search.php
+++ search.php
@@ -2,17 +2,13 @@
 include('./dbconnect.php');
 if(isset($_POST['userName'])){
       $id = $_POST['userName'];
-    // if($_SESSION['id'] == $id){
-      $sql = "SELECT * FROM user WHERE user_id = '$id'";
-      $stmt = $pdo->prepare($sql);
-      $stmt -> execute();
-      $result = $stmt->fetch();
-      $userNumber = $result['user_number'];
-      $sql = "SELECT * FROM problem_check WHERE user_number = $userNumber";
+
+      $sql = "SELECT * FROM problem_check WHERE user_number = $id";
       $stmt = $pdo->prepare($sql);
       $stmt -> execute();
       $result2 = $stmt->fetchAll(PDO::FETCH_ASSOC);
       $problem['problem'] = true;
+
       if($result2){
         foreach($result2 as $problemId){
           if($problemId['is_correct'] == 1){
@@ -28,9 +24,6 @@ if(isset($_POST['userName'])){
 
       $result = json_encode($problem);
       echo $result;
-    // } else{
-    //   echo '접속된 아이디가 다릅니다';
-    // }
 }else{
   echo '오류';
 }
